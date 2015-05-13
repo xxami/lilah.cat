77ea009d0eaecdc87acfde0ac11d9d364426eb51
ami <ami@lilah.cat>
2015-05-13 22:02:05 +0100

    added ajax for retrieving content

---
+++ get_content.php
@@ -0,0 +1,8 @@
+<?php
+
+	require_once('db.php');
+
+	echo '{"status": "success"}';
+
+?>
+
--- lilah.js
+++ lilah.js
@@ -5,7 +5,27 @@
 
 $(document).ready(function() {
 
-	/* */
+	$('#content_next').click(function() {
+		$.ajax({
+			url: 'get_content.php',
+			dataType: 'json',
+			timeout: 7000,
+		}).success(function(data) {
+			if (typeof(data['status']) == 'undefined') {
+				/**
+				 * handle connection fail
+				 */
+				return;
+			}
+			/**
+			 * update content
+			 */
+		}).error(function(data) {
+			/**
+			 * handle connection fail
+			 */
+		});
+	});
 
 });
 
